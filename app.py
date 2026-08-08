ef5cf6e40b109ae745ab3c903e0002998e914c41
Arul Prasanth <arulprasanthds@gmail.com>
2026-08-09 03:08:31 +0530

    Fix Render Flask app startup

--- app.py
+++ app.py
@@ -100,11 +100,12 @@ def create_app():
 # Run Application
 # ======================================
 
+app = create_app()
+
 if __name__ == "__main__":
-
-    app = create_app()
-
     app.run(
+        host="0.0.0.0",
+        port=5000,
         debug=True,
         use_reloader=False
     )
